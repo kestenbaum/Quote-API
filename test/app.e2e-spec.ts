1bfd80d8a257ad2647e1a383def781b49cc3d657
Oleksii <kespa71for@gmail.com>
2025-11-17 15:24:49 +0100

    refactor: test for user-module

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,14 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { UserModule } from './../src/users/user.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [UserModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
