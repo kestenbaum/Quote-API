403c89ddbaaf2ce6ea3e334821d6a9718421afa2
Oleksii <kespa71for@gmail.com>
2025-11-18 16:26:59 +0100

    fix: fixed migration to db

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,11 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
-import * as path from 'path';
-
-const rootDir = path.resolve(__dirname, '..');
 
 export default new DataSource({
   type: 'sqlite',
   database: 'database.sqlite',
-
-  entities: [path.join(rootDir, 'src/quote/entity/*.entity{.ts,.js}')],
-
-  migrations: [path.join(rootDir, 'src/quote/database/migrations/*.{ts,.js}')],
-
+  entities: ['src/**/*.entity.ts'],
+  migrations: ['src/**/migrations/*.ts'],
   synchronize: false,
   logging: true,
 });
