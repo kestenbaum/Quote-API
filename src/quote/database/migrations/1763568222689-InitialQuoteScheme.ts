cd4c05fc7e09d63fcc2e769f55ca2a81a904bed0
Oleksii <kespa71for@gmail.com>
2025-11-19 17:08:09 +0100

    fix: fixed query to quote

--- src/quote/database/migrations/1763568222689-InitialQuoteScheme.ts
+++ src/quote/database/migrations/1763568222689-InitialQuoteScheme.ts
@@ -1,13 +1,15 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class InitialUserSchema1763559605997 implements MigrationInterface {
-    name = 'InitialUserSchema1763559605997'
+export class InitialQuoteScheme1763568222689 implements MigrationInterface {
+    name = 'InitialQuoteScheme1763568222689'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "user" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar(50) NOT NULL, "email" varchar(100) NOT NULL, CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"))`);
+        await queryRunner.query(`CREATE TABLE "quote_entity" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "quote" varchar(100) NOT NULL, "author" varchar(20) NOT NULL)`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "quote_entity"`);
         await queryRunner.query(`DROP TABLE "user"`);
     }
 
